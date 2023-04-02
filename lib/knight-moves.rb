9f177b67e352c1c4e2a085ca824ee0174b18a811
menchxCodes <43462510+menchxCodes@users.noreply.github.com>
2023-04-02 13:53:42 +0330

    First Implementation of Knight Piece & #pos(piece)

--- lib/knight-moves.rb
+++ lib/knight-moves.rb
@@ -2,7 +2,8 @@ class Board
   attr_accessor :board
 
   def initialize
-    string = ' abcdefgh'
+    # string = ' abcdefgh'
+    string = ' 12345678'
     @board = Array.new(9) { Array.new(9) }
     @board.each_with_index do |row, row_index|
       row.each_with_index do |_col, col_index|
@@ -15,12 +16,25 @@ class Board
     end
   end
 
+  def pos(piece)
+    
+
+    @board.each_with_index do |row, index|
+      position = row.find_index(piece)
+      return [index, position] unless position.nil?
+    end
+  end
+
   def print_board
     puts "\n"
     @board.reverse.each_with_index do |row, _row_index|
       string = ''
       row.each_with_index do |col, _col_index|
-        string.concat(" #{col} |")
+        if col.instance_of?(Knight)
+          string.concat(" #{col.avatar} |") if col.instance_of?(Knight)
+        else
+          string.concat(" #{col} |")
+        end
       end
       puts string
       puts '------------------------------------'
@@ -29,11 +43,23 @@ class Board
   end
 end
 
+class Knight
+  attr_reader :avatar
+  def initialize
+    knight_utf = "\u265e"
+    @avatar = "#{knight_utf.encode('utf-8')}"
+    # @avatar = "K"
+  end
+end
 #-- TESTS --
 game_board = Board.new
 # game_board.board[1][1] = "x"
 # game_board.board[0][5] = 5
-game_board.board[8][8] = 'x'
-game_board.print_board
+
 
 # game_board.print_board
+knight = Knight.new
+game_board.board[5][3] = knight
+game_board.print_board
+# pp game_board.board[1..8][1..8]
+p game_board.pos(knight)
